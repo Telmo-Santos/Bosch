bc2b7a467aa8e11976250312df1f35c782c204fc
Telmo Santos Carneiro <telmo2811@gmail.com>
2022-05-09 19:54:28 +0200

    Layed project out. Create Database that i will need.

--- BoschWebsite/models.py
+++ BoschWebsite/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
+
 # Create your models here.
+
+# Saving Data of a Driver
+class Driver(models.Model):
+    name_text = models.CharField(max_length=100)
+    age = models.IntegerField(default=0)
+    wins = models.IntegerField(default=0)
+    poles = models.IntegerField(default=0)
+    titles = models.IntegerField(default=0)
+    start_date = models.DateTimeField('date published')
+    driver_image = models.ImageField(upload_to='uploads/')
+
+    # Instead of showing Driver (1) in shell now it shows e.g. Driver Lando Norris
+    # Same in admin website when Deleting (http://127.0.0.1:8000/admin/)
+    def __str__(self):
+        return self.name_text
+
+
+# Saving Data of a Team
+class Team(models.Model):
+    name_text = models.CharField(max_length=50)
+    symbol = models.ImageField(upload_to='uploads/')
+    wins = models.IntegerField(default=0)
+    poles = models.IntegerField(default=0)
+    boss = models.CharField(max_length=100)
+    start_date = models.DateTimeField('date published')
+
+    # Instead of showing Team (1) in shell now it shows e.g. Team McLaren
+    # Same in admin website when Deleting (http://127.0.0.1:8000/admin/)
+    def __str__(self):
+        return self.name_text
+
+
+# Saving Data of a Track
+class Track(models.Model):
+    name_text = models.CharField(max_length=50)
+    laps = models.IntegerField(default=0)
+    lapRecord = models.TimeField()
+    sector1 = models.TimeField()
+    sector2 = models.TimeField()
+    sector3 = models.TimeField()
+    length = models.IntegerField(default=0)
+    layout = models.ImageField(upload_to='uploads/')
+
+    # Instead of showing Track (1) in shell now it shows e.g. Track Melbourne
+    # Same in admin website when Deleting (http://127.0.0.1:8000/admin/)
+    def __str__(self):
+        return self.name_text
