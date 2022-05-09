82e276756edf98d852d12206a1a680461b5a7aa3
Telmo Santos Carneiro <telmo2811@gmail.com>
2022-05-09 21:15:22 +0200

    Created some html files.

--- BoschWebsite/urls.py
+++ BoschWebsite/urls.py
@@ -1,6 +1,17 @@
 from django.urls import path
 from . import views
 
+app_name = 'BoschWebsite'
 urlpatterns = [
-    path('', views.index, name='index')
+    # /BoschWebsite/
+    path('', views.index, name='index'),
+    # /BoschWebsite/2/
+    path('<int:driver_id>/', views.driverdetail, name='driverdetail'),
+    # /BoschWebsite/2/results/
+    path('<int:driver_id>/results/', views.results, name='results'),
+    # /BoschWebsite/2/vote/
+    path('<int:driver_id>/vote/', views.vote, name='vote'),
+
+    # path('<int: team_id>', views.detail, name='detail'),
+    # path('<int: track_id>', views.detail, name='detail'),
 ]
--- BoschWebsite/views.py
+++ BoschWebsite/views.py
@@ -1,7 +1,36 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, Http404
+from django.template import loader
+from .models import Driver
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World. You´re at the BoschWebsite index.")
+    latest_driver_list = Driver.objects.order_by('name_text')
+
+    # context is here so that we can access variables in html
+    context = {
+        'latest_driver_list': latest_driver_list,
+    }
+    return render(request, 'BoschWebsite/index.html', context)
+
+
+# return HttpResponse("Hello World. You´re at the BoschWebsite index.")
+
+
+# Details about Driver are shown here
+# If Driver does not exist, Exception is thrown
+def driverdetail(request, driver_id):
+    driver = get_object_or_404(Driver, pk=driver_id)
+    return render(request, 'BoschWebsite/driverdetail.html', {'driver': driver})
+
+
+# From tutorial
+def results(request, driver_id):
+    response = "You´re looking at the response of Driver %s."
+    return HttpResponse(response % driver_id)
+
+
+# From tutorial
+def vote(request, driver_id):
+    return HttpResponse("You´re voting on Driver %s." % driver_id)
