c66ce2f8ae6bd8356f6d6b9ba9e35e2aeacd63e2
Telmo Santos Carneiro <telmo2811@gmail.com>
2022-05-10 20:07:24 +0200

    Made Changes to Database. And filled DB with Data.

--- BoschWebsite/models.py
+++ BoschWebsite/models.py
@@ -3,22 +3,6 @@ from django.db import models
 
 # Create your models here.
 
-# Saving Data of a Driver
-class Driver(models.Model):
-    name_text = models.CharField(max_length=100)
-    age = models.IntegerField(default=0)
-    wins = models.IntegerField(default=0)
-    poles = models.IntegerField(default=0)
-    titles = models.IntegerField(default=0)
-    start_date = models.DateTimeField('date published')
-    driver_image = models.ImageField(upload_to='uploads/')
-
-    # Instead of showing Driver (1) in shell now it shows e.g. Driver Lando Norris
-    # Same in admin website when Deleting (http://127.0.0.1:8000/admin/)
-    def __str__(self):
-        return self.name_text
-
-
 # Saving Data of a Team
 class Team(models.Model):
     name_text = models.CharField(max_length=50)
@@ -29,6 +13,24 @@ class Team(models.Model):
     start_date = models.DateTimeField('date published')
 
     # Instead of showing Team (1) in shell now it shows e.g. Team McLaren
+    # Same in admin website when Deleting (http://127.0.0.1:8000/admin/)
+    def __str__(self):
+        return self.name_text
+
+    # Saving Data of a Driver
+
+
+class Driver(models.Model):
+    name_text = models.CharField(max_length=100)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, default="", null=True, blank=True)
+    age = models.IntegerField(default=0)
+    wins = models.IntegerField(default=0)
+    poles = models.IntegerField(default=0)
+    titles = models.IntegerField(default=0)
+    start_date = models.DateTimeField('date published')
+    driver_image = models.ImageField(upload_to='uploads/')
+
+    # Instead of showing Driver (1) in shell now it shows e.g. Driver Lando Norris
     # Same in admin website when Deleting (http://127.0.0.1:8000/admin/)
     def __str__(self):
         return self.name_text
