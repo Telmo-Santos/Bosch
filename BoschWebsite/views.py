346a6c2f0e3259e12002449dfcb42abcc364fe89
Telmo Santos Carneiro <telmo2811@gmail.com>
2022-05-15 11:43:51 +0200

    Took out context from index.

--- BoschWebsite/views.py
+++ BoschWebsite/views.py
@@ -7,19 +7,6 @@ from .models import Team, Driver, Track
 
 # Create your views here.
 def index(request):
-    # Here we are ordering the drivers by their team
-    latest_driver_list = Driver.objects.order_by('team')
-    # Ordering Teams by number of wins (descending)
-    latest_team_list = Team.objects.order_by('-wins')
-    # Ordering Tracks by their schedule in 2021
-    latest_track_list = Track.objects.order_by('id')
-
-    # context is here so that we can access variables in html
-    context = {
-        'latest_driver_list': latest_driver_list,
-        'latest_team_list': latest_team_list,
-        'latest_track_list': latest_track_list,
-    }
     return render(request, 'BoschWebsite/index.html')
 
 
